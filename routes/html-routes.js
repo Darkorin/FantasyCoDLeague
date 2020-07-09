6bf471c899c1c78bb7276ab5990bb1ed1bfc97d2
David Stewart <darkorin00@hotmail.com>
2020-07-09 19:41:01 -0400

    First stage of refactoring

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,26 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
+
+  // Set Handlebars as the default templating engine.
+  app.use(express.urlencoded({ extended: false }))
+  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  app.set("view engine", "handlebars");
+  app.use(express.static(path.join(__dirname, 'app/public')));
+  app.use(express.json());
+
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup");
   });
 
   app.get("/login", (req, res) => {
@@ -18,12 +28,12 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render("members");
   });
 };

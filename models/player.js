05e1d4725d19f5793a9b261b695c14c9bf31d081
David Stewart <darkorin00@hotmail.com>
2020-07-09 21:06:39 -0400

    added base player model

--- models/player.js
+++ models/player.js
@@ -1,12 +1,13 @@
-module.exports = function (sequelize, DataTypes) {
-    const User = sequelize.define("User", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        // Add the rest of the stats you pick to display below.
-        // Syntax is statName: {
-        //                    type: DataTypes.STRING or DataTypes.Decimal
-        //           },
-    })
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+      name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      // Add the rest of the stats you pick to display below.
+      // Syntax is statName: {
+      //                    type: DataTypes.STRING or DataTypes.Decimal,
+      //                    allowNull: true
+      //           },
+  })
 }
